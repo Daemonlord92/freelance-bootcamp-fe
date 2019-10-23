95cf8e484ffc2d4e59a1d32912300f87d6b4f12c
Matthew Martin <zhintateo19924559@gmail.com>
2019-10-23 06:12:28 -0700

    Added the Documents Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { DocumentsComponent } from './documents/documents.component';
 
 
 const routes: Routes = [
-{ path: '', redirectTo: '/app-homepage', pathMatch: 'full' },
-];
+	{ path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomepageComponent },
+  { path: 'documents', component: DocumentsComponent },
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
