693afd34cb6321e893dd2dd693a34cc270dd920f
Matthew Martin <zhintateo19924559@gmail.com>
2019-10-23 06:53:58 -0700

    Test data for documents

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,9 +1,31 @@
 import { Component } from '@angular/core';
+import { Document } from './document';
 
 @Component({
 	selector: 'documents',
 	templateUrl: 'documents.component.html'
 })
 export class DocumentsComponent {
+	documents: Document[] = [
+	{
+		title: "testDoc1",
+		description: "A test Document",
+		file_url: "http://www.3jmcoding.com",
+		updated_at: '10/23/19',
+		image_url: 'http://www.3jmcoding.com',
+	},{
+		title: "testDoc2",
+		description: "A test Document",
+		file_url: "http://www.3jmcoding.com",
+		updated_at: '10/23/19',
+		image_url: 'http://www.3jmcoding.com',
+	},{
+		title: "testDoc3",
+		description: "A test Document",
+		file_url: "http://www.3jmcoding.com",
+		updated_at: '10/23/19',
+		image_url: 'http://www.3jmcoding.com',
+	},
 
+	]
 }
