463c061f2ae2ad64fdb2024ea18b63a0349e778d
Matthew Martin <zhintateo19924559@gmail.com>
2019-11-11 06:32:48 -0800

    Sucessfully Impletemented Bootstrap into angular through ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,7 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { DocumentsComponent } from './documents/documents.component'
 import { ProposalListComponent } from './proposal/proposal-list.component';
 import { ProposalNewComponent } from './proposal/proposal-new.component';
+import { ProposalShowComponent } from './proposal/proposal-show.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +18,14 @@ import { ProposalNewComponent } from './proposal/proposal-new.component';
     DocumentsComponent,
     ProposalListComponent,
     ProposalNewComponent,
+    ProposalShowComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
