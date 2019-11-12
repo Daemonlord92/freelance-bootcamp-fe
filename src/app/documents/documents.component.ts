32d05d8c513998d6ebe466744966c8efec1eb9f4
Matthew Martin <zhintateo19924559@gmail.com>
2019-11-12 14:10:17 -0800

    added card interface to DocumentComponent

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -13,19 +13,19 @@ export class DocumentsComponent {
 		description: "A test Document",
 		file_url: "http://www.3jmcoding.com",
 		updated_at: '10/23/19',
-		image_url: 'http://www.3jmcoding.com',
+		image_url: 'https://www.splitshire.com/wp-content/uploads/2018/08/SplitShire-18-07913.jpg',
 	},{
 		title: "testDoc2",
 		description: "A test Document",
 		file_url: "http://www.3jmcoding.com",
 		updated_at: '10/23/19',
-		image_url: 'http://www.3jmcoding.com',
+		image_url: 'https://www.splitshire.com/wp-content/uploads/2018/08/SplitShire-18-07913.jpg',
 	},{
 		title: "testDoc3",
 		description: "A test Document",
 		file_url: "http://www.3jmcoding.com",
 		updated_at: '10/23/19',
-		image_url: 'http://www.3jmcoding.com',
+		image_url: 'https://www.splitshire.com/wp-content/uploads/2018/08/SplitShire-18-07913.jpg',
 	},
 
 	]
