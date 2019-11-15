4b26c5b15025a55e3c7ce2682fa6adc6497697bb
Matthew Martin <zhintateo19924559@gmail.com>
2019-11-15 10:20:16 -0800

    Fixed bug with visaul highlight

--- src/app/proposal/proposal-show.component.ts
+++ src/app/proposal/proposal-show.component.ts
@@ -4,7 +4,8 @@ import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
 	selector: 'proposal-show',
-	templateUrl: 'proposal-show.component.html'
+	templateUrl: 'proposal-show.component.html',
+	styleUrls: ['proposal.component.css']
 })
 export class ProposalShowComponent implements OnInit {
 	id: number;
